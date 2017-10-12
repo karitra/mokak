d9c3ca2147871c5b1964f7541f779d74f09ebf3c
karitra <karev.alexandre@gmail.com>
2017-10-12 19:49:13 +0300

    feat: self status

--- mokak/web.py
+++ mokak/web.py
@@ -11,8 +11,25 @@ def make_status_web_handler(shared_status, path, port, address=''):
 
 
 class _StatusHandler(web.RequestHandler):
+    TASK_NAME = 'status_handler'
+
     def initialize(self, shared_status):
         self.shared_status = shared_status
+        self.status = shared_status.register(_StatusHandler.TASK_NAME)
+        self.requests_count = 0
 
     def get(self):
+        self._inc_count()
         self.write(self.shared_status.as_dict())
+        self._drop_count()
+
+    def _inc_count(self):
+        self.requests_count += 1
+        self.status.mark_ok(
+            'processing {} request(s)'.format(self.requests_count))
+
+    def _drop_count(self):
+        self.requests_count -= 1
+        if self.requests_count <= 0:
+            self.requests_count
+            self.status.mark_ok('idle')
